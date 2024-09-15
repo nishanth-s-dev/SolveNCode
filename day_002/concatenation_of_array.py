90f3bf2a54e0a2ba75383b13ea3394f3946498fb
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-09-15 06:29:52 +0530

    day_002 - problem 7 [concatenation of array]

---
+++ day_002/concatenation_of_array.py
@@ -0,0 +1,11 @@
+# Platform          : Leetcode
+# Problem Number    : 1929
+# Problem Name      : Concatenation of Array
+# Problem Topics    : Array, Simulation
+# Problem URL       : https://leetcode.com/problems/concatenation-of-array
+
+def concatenate_arrays(nums):
+    return nums + nums
+
+if __name__ == "__main__":
+    print(concatenate_arrays([1, 2, 3, 4, 5]))
