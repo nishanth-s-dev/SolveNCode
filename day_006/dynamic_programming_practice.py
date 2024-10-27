00b612a4db9c0addc5fcef11d56d5fa5aae4bf6b
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-10-28 00:52:53 +0530

    practice dynamic programing - Rafactoring

--- day_006/dynamic_programming_practice.py
+++ day_006/dynamic_programming_practice.py
@@ -1,25 +1,26 @@
 def fib(n, memo = dict()):
-    if n <= 2:
-        return n
+    if n <= 2: return n
+
     if n not in memo:
         memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
     return memo[n]
 
 
-def gridTraveler(row, col, memo = dict()):
-    if row == 1 and col == 1:
-        return 1
-    if row < 1 or col < 1:
-        return 0
+def gridTraveler(row, col, memo = None):
+    if memo is None: memo = dict()
+    if row == 1 and col == 1: return 1
+    if row < 1 or col < 1: return 0
 
-    if (row, col) not in memo:
-        memo[(row, col)] = gridTraveler(row - 1, col, memo) + gridTraveler(row, col - 1, memo)
+    key = (min(row, col), max(row, col))
 
-    return memo[(row, col)]
+    if key not in memo:
+        memo[key] = gridTraveler(row - 1, col, memo) + gridTraveler(row, col - 1, memo)
+
+    return memo[key]
 
 
 def main():
-    print(gridTraveler(22, 33))
+    print(gridTraveler(18, 18))
 
 
 if __name__ == '__main__':
